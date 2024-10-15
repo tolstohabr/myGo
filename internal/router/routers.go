1262537b9e0392d8f49ddb50131fce30dc0561bb
Temirlan Ragimov <temirlan.official@mail.ru>
2024-10-15 22:00:39 +0300

    без говна

--- internal/router/routers.go
+++ internal/router/routers.go
@@ -7,8 +7,8 @@ import (
 )
 
 func RegisterRoutes(r *gin.Engine, handler *handler.Handler) {
-	r.GET("/banners", handler.GetBanners)             // Регистрируем GET
-	r.POST("/banners/create", handler.CreateBanner)   // Регистрируем POST
-	r.PUT("/banners/update", handler.UpdateBanner)    // Регистрируем PUT
-	r.DELETE("/banners/delete", handler.DeleteBanner) // Регистрируем DELETE
+	r.GET("/banners", handler.GetBanners)
+	r.POST("/banners/create", handler.CreateBanner)
+	r.PUT("/banners/update", handler.UpdateBanner)
+	r.DELETE("/banners/delete", handler.DeleteBanner)
 }
