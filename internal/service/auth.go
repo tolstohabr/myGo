9da8fb00beb11f832a5d8e6dc481f5e9de7f0afb
Temirlan Ragimov <temirlan.official@mail.ru>
2024-11-15 06:08:43 +0300

    после созвона 15112024

---
+++ internal/service/auth.go
@@ -0,0 +1,40 @@
+//TODO: методы сделать из функций
+//jwtтокен брать из конфига и передавать в констуркторе
+
+package service
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var jwtKey = []byte("JWT_SECRET_KEY") //небезопасно
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func GenerateJWT(username string) (string, error) {
+	expirationTime := time.Now().Add(1 * time.Hour)
+	claims := &Claims{
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
+
+func ValidateJWT(tokenStr string) (*Claims, error) {
+	claims := &Claims{}
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil || !token.Valid {
+		return nil, err
+	}
+	return claims, nil
+}
